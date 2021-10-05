2ab50456c69aa3747833becfd7b752e539b45ec5
tomekpajak <44640274+tomekpajak@users.noreply.github.com>
2021-10-05 23:14:38 +0200

    added modules pvm-copy-folders and pvm-invoke-cmds (#2)

---
+++ terraform/proxmox/modules/pvm-copy-folders/pvm-copy-folders.tf
@@ -0,0 +1,22 @@
+resource "null_resource" "pvm_copy_folders" {
+  for_each = var.folders
+
+  connection {
+    timeout     = "10s"
+    type        = "ssh"
+    host        = var.pve.host
+    user        = var.pve.ssh_user
+    private_key = var.pve.ssh_key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p ${each.value.target}"
+    ]
+  }
+
+  provisioner "file" {
+    source      = each.value.source
+    destination = each.value.target
+  }  
+}
---
+++ terraform/proxmox/modules/pvm-invoke-cmds/pvm-invoke-cmds.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "pvm_invoke_cmds" {
+  connection {
+    timeout     = "10s"
+    type        = "ssh"
+    host        = var.pve.host
+    user        = var.pve.ssh_user
+    private_key = var.pve.ssh_key
+  }
+
+  provisioner "remote-exec" {
+    inline = var.commands
+  }    
+}
